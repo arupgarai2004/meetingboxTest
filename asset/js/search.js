890910367e285aa61bb2844f1373e6e26ad46134
arupgarai2004 <arupgarai2004@gmail.com>
2021-07-05 10:05:52 +0530

    Add files via upload

--- asset/js/search.js
+++ asset/js/search.js
@@ -1,1 +1,25 @@
-
+$(function() {
+	$("#frm_search").on("submit", function(event) {
+		event.preventDefault();
+		var address = $('input[name=address]').val() //to get address 
+		if(address.length > 0) {
+			$('#result_container').html('Loading...');
+			$.post("searchAddress.php", {
+				searchAddress: address
+			}, function(result) {
+				console.log(result);
+				var resultContent = '';
+				$.each(result, function(key, val) {
+					$.each(val, function(name, response) {
+						resultContent += "<div class='flex-container'><div class='col addressTxt'>For Address " + address + " : <br/>" + name + ":</div>";
+						$.each(response, function(ofSet, res) {
+							resultContent += "<div class='col'>Latitude : " + res.lat + "  <br/>Longitude : " + res.lon + "</div>";
+						});
+						resultContent += "</div>";
+					});
+				});
+				$("#result_container").html(resultContent);
+			}, 'json');
+		}
+	});
+})
